be17e1aeea8d3c506aa06ac3a80ea7a8d6b8c3bf
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-05-06 10:41:10 +0100

    Feat: added endpoints for store creation and retrieval of store item by type

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import jsonify, request
 from app import app
 
@@ -6,17 +7,26 @@ stores = [
     {
         'id': 1,
         'name': 'Ali Stores',
-        'type': 'Bookshop'
+        'type': 'Bookshop',
+        'items': []
     },
     {
         'id': 2,
         'name': 'Bala Stores',
-        'type': 'Supermarket'
+        'type': 'Supermarket',
+        'items': []
     },
     {
         'id': 3,
         'name': 'Iya Gbogo Stores',
-        'type': 'Confectionery Store'
+        'type': 'Confectionery Store',
+        'items': []
+    },
+    {
+        "id": 4,
+        "items": [],
+        "name": "Sola Enterprises",
+        "type": "building materials"
     }
 ]
 
@@ -32,10 +42,28 @@ def get_stores():
 
 @app.route('/stores/<int:id>')
 def get_store(id):
-    store_item = None
     for item in stores:
         if item['id'] == int(id):
-            print('item: ', item)
-            store_item = item
-            
-    return store_item
+            return jsonify({'store': item})
+    return jsonify({'message': 'store not found'})
+
+@app.route('/stores/<string:type>/item')
+def get_store_by_type(type):
+    for item in stores:
+        if item['type'].lower() == type.lower():
+            return jsonify({'store': item})
+    return jsonify({'message': 'store with that type could not be found'}) 
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    index_num = len(stores) + 1
+    new_store = {
+        'id': index_num,
+        'name': request_data['name'],
+        'type': request_data['type'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify({ 'message': 'store created', 'store': new_store })
